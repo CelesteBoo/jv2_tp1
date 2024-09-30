36f0db1b1200897fbc138eaf20240a61e411a866
Céleste <deucefan33@gmail.com>
2024-09-30 09:15:44 -0400

    mouvement joueur de base

--- Assets/Scripts/Entities/Player.cs
+++ Assets/Scripts/Entities/Player.cs
@@ -1,16 +1,59 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("Movement")]
+    [SerializeField] private float speed = 30f;
+    [SerializeField] private float rotationSpeed = 0.1f;
+
+    [Header("Inputs")]
+    [SerializeField] private InputActionReference moveAction;
+
+    private CharacterController controller;
+    private Transform cameraTransform;
+
+    private void Awake()
     {
-        
+        cameraTransform = Camera.main?.transform;
+        controller = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        Vector3 moveVector = Vector3.zero;
+
+        //Copié de l'exercice cinemachine
+        // Get directions.
+        var relativeTransform = cameraTransform ?? transform;
+        var forward = relativeTransform.forward;
+        var right = relativeTransform.right;
+
+        // Remove Y component from vectors.
+        forward.y = 0;
+        right.y = 0;
+
+        // Calculate movement direction.
+        var moveInput = moveAction.action.ReadValue<Vector2>();
+
+        //Apply gravity.
+        if (!controller.isGrounded)
+        {
+            moveVector += Physics.gravity;
+        }
+
+        if (moveInput != Vector2.zero)
+        {
+            var moveDirection = forward * moveInput.y + right * moveInput.x;
+            var lookRotation = Quaternion.LookRotation(moveDirection);
+
+            moveVector += moveDirection * speed;
+            transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, rotationSpeed);
+        }
+
+        // Apply movement.
+        controller.Move(moveVector * Time.deltaTime);
+
     }
 }
